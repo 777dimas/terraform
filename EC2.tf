8a6cb16a6be45fceb0c6c78c96379fc53873c867
Dmitry <dmitry.b@rabbitsup.com>
2019-11-01 10:38:16 +0200

    add tags

--- EC2.tf
+++ EC2.tf
@@ -3,7 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "linux" {
-  ami = "ami-0cc0a36f626a4fdf5"
+  ami           = "ami-0cc0a36f626a4fdf5"
   instance_type = "t3.micro"
+
+  tags = {
+    Name    = "Linux server"
+    Owner   = "DM"
+    Project = "Test_teraaform"
+  }
 }
 
