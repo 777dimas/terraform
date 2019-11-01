ecdf24f114c96f8c309d5dd1c1970c87432cf641
Dmitry <dmitry.b@rabbitsup.com>
2019-11-01 10:23:45 +0200

    use export variable

--- EC2.tf
+++ EC2.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
   region = "eu-central-1"
 }
+
 
 resource "aws_instance" "linux" {
   ami = "ami-0cc0a36f626a4fdf5"
