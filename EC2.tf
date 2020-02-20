83edcbc613e20d5112f5a68a36def19afd064c85
Dmitry <dmitry.b@rabbitsup.com>
2020-02-20 09:39:44 +0200

    dynamic

--- EC2.tf
+++ EC2.tf
@@ -19,19 +19,15 @@ resource "aws_security_group" "linux_web" {
   name        = "Security Group"
   description = "Allow inbound traffic"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8888
-    to_port     = 8888
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  dynamic "ingress" {
+    for_each = ["22", "8888"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }	
+  }   
 
   egress {
     from_port   = 0
