482c513c766f9594032812f1c9ab4ba1e1a843ef
kriptex <kriptexm@gmail.com>
2020-03-06 13:53:43 +0200

    add "aws elastic ip" and "lifecycle"

--- EC2.tf
+++ EC2.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "awp_eip" "my_static_ip" {
+  instance = aws_instance.Ubuntu_docker_jenkins.id
+}
+
 resource "aws_instance" "Ubuntu_docker_jenkins" {
   ami                    = "ami-0cc0a36f626a4fdf5"
   instance_type          = "t3.micro"
@@ -12,6 +16,10 @@ resource "aws_instance" "Ubuntu_docker_jenkins" {
   tags = {
     Name    = "Ubuntu server"
     Project = "Terraform"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
